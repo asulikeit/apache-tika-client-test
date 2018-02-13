6ede41addbfa613e88e95c8ce529a65b60e22f78
Daniel <senticat.lee@gmail.com>
2018-02-13 19:59:22 +0900

    update image scan

--- src/main/java/kr/rootuser/tika/client/test/TikaClientTestImage.java
+++ src/main/java/kr/rootuser/tika/client/test/TikaClientTestImage.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.apache.tika.config.TikaConfig;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
@@ -19,51 +20,55 @@ import org.xml.sax.SAXException;
  * 
  * @author Daniel
  *
- * Reference:
- * https://stackoverflow.com/questions/41985163/unable-to-extract-content-directly-from-scanned-pdf-using-apache-tika-but-work
- * https://digi.bib.uni-mannheim.de/tesseract/ - windows
+ *         Reference:
+ *         https://stackoverflow.com/questions/41985163/unable-to-extract-content-directly-from-scanned-pdf-using-apache-tika-but-work
+ *         https://digi.bib.uni-mannheim.de/tesseract/ - windows
  */
 public class TikaClientTestImage {
 
 	// private static String filename = "ORG_english_contract.pdf";
 	private static String filename = "SCAN_english_contract.pdf";
-//	private static String tesseract = "C:\\Program Files (x86)\\Tesseract-OCR";
-	private static String tesseract = "D:\\Program\\tesseract-Win64";
+	// private static String filename = "SCAN_english_contract.jpg";
+	private static String tesseract = "C:\\Program Files (x86)\\Tesseract-OCR";
+	// private static String tesseract = "D:\\Program\\tesseract-Win64";
 	private static String tessdata = "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata";
 
 	public static void main(String[] args) throws IOException, SAXException, TikaException {
 		FileInputStream stream = getFilePath();
 		BodyContentHandler handler = new BodyContentHandler(Integer.MAX_VALUE);
 
-		Parser parser = new AutoDetectParser();
-		
+		TikaConfig config = TikaConfig.getDefaultConfig();
+		Parser parser = new AutoDetectParser(config);
+		Metadata metadata = new Metadata();
+
 		TesseractOCRConfig ocrConfig = new TesseractOCRConfig();
+		ocrConfig.setLanguage("eng");
 		ocrConfig.setTesseractPath(tesseract);
 		ocrConfig.setTessdataPath(tessdata);
 		ocrConfig.setMaxFileSizeToOcr(Integer.MAX_VALUE);
-		
+
 		PDFParserConfig pdfConfig = new PDFParserConfig();
 		pdfConfig.setExtractInlineImages(true);
 		pdfConfig.setExtractUniqueInlineImagesOnly(false);
 
 		ParseContext parseContext = new ParseContext();
-		parseContext.set(TesseractOCRConfig.class, ocrConfig);
-		parseContext.set(PDFParserConfig.class, pdfConfig);
 		// need to add this to make sure recursive parsing happens!
 		parseContext.set(Parser.class, parser);
+		parseContext.set(TesseractOCRConfig.class, ocrConfig);
+		parseContext.set(PDFParserConfig.class, pdfConfig);
 
-		Metadata metadata = new Metadata();
 		parser.parse(stream, handler, metadata, parseContext);
-//		System.out.println(metadata);
+		// System.out.println(metadata);
 		String content = handler.toString();
 		System.out.println("===============");
 		System.out.println(content);
 		System.out.println("Done");
-		
-		/*ContentHandler contenthandler = new BodyContentHandler();
-		Metadata metadata = new Metadata();
-		PDFParser pdfparser = new PDFParser();
-		pdfparser.parse(is, contenthandler, metadata, new ParseContext());*/
+
+		/*
+		 * ContentHandler contenthandler = new BodyContentHandler(); Metadata metadata =
+		 * new Metadata(); PDFParser pdfparser = new PDFParser(); pdfparser.parse(is,
+		 * contenthandler, metadata, new ParseContext());
+		 */
 	}
 
 	private static FileInputStream getFilePath() throws FileNotFoundException {
